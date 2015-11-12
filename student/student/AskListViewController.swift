64569623fd68a7f5543e7ce79e8cde3189d3fddb
mc <zjueman@qq.com>
2015-11-12 20:57:33 +0800

    初始框架版本

---
+++ student/student/AskListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AskListViewController.swift
+//  student
+//
+//  Created by zjueman on 15/11/12.
+//  Copyright © 2015年 singlu. All rights reserved.
+//
+
+import UIKit
+
+class AskListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ student/student/EtaskDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  EtaskDetailViewController.swift
+//  student
+//
+//  Created by zjueman on 15/11/12.
+//  Copyright © 2015年 singlu. All rights reserved.
+//
+
+import UIKit
+
+class EtaskDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
