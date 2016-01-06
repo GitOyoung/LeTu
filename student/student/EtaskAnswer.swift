24e4f408f8fe0dbc3206b6b78e7cb548d4703eb4
zhaoheqiang <quiniton@163.com>
2016-01-06 10:00:26 +0800

    fix_init_andwer

--- student/student/EtaskAnswer.swift
+++ student/student/EtaskAnswer.swift
@@ -10,41 +10,21 @@ import Foundation
 
 class EtaskAnswer: NSObject {
 
-    var answer:String
+    var answer:String = ""
     var answerHistory:[String]?
     var checkImgs:[String]?
-    var checkStatus:Bool
+    var checkStatus:Bool = false
     var checkTime:NSDictionary?
-    var costTime:Int16
+    var costTime:Int = 0
     var finishTime:NSDictionary?
-    var isRight:Bool
+    var isRight:Bool = false
     var listAnswer:[AnyObject]?
-    var ordinal:Int
-    var questionId:String
-    var score:Int
-    var startTime:NSDictionary
-    var type:Int
-    var viewedTime:Int16
-    
-    init(answer:String,answerHistory:[String]?,checkImgs:[String]?,checkStatus:Bool,checkTime:NSDictionary?,costTime:Int16,finishTime:NSDictionary?,isRight:Bool,listAnswer:[AnyObject]?,ordinal:Int,questionId:String,score:Int,startTime:NSDictionary?,type:Int,viewedTime:Int16) {
-        self.answer = answer
-        self.answerHistory = answerHistory
-        self.checkImgs = checkImgs
-        self.checkStatus = checkStatus
-        self.checkTime = checkTime
-        self.costTime = costTime
-        self.finishTime = finishTime
-        self.isRight = isRight
-        self.listAnswer = listAnswer
-        self.ordinal = ordinal
-        self.questionId = questionId
-        self.score = score
-        self.startTime = startTime!
-        self.type = type
-        self.viewedTime = viewedTime
-        super.init()
-
-    }
+    var ordinal:Int = 0
+    var questionId:String = ""
+    var score:Int = 0
+    var startTime:NSDictionary?
+    var type:Int = 0
+    var viewedTime:Int = 0
     
     func toDictionary() -> NSDictionary{
         var answerDic = [String:AnyObject]()
@@ -53,7 +33,7 @@ class EtaskAnswer: NSObject {
         answerDic["checkImgs"] = self.checkTime
         answerDic["checkStatus"] = self.checkStatus
         answerDic["checkTime"] = self.checkTime
-        answerDic["costTime"] = NSNumber(short: self.costTime)
+        answerDic["costTime"] = self.costTime
         answerDic["finishTime"] = self.finishTime
         answerDic["isRight"] = self.isRight
         answerDic["listAnswer"] = self.listAnswer
@@ -62,7 +42,7 @@ class EtaskAnswer: NSObject {
         answerDic["score"] = self.score
         answerDic["startTime"] = self.startTime
         answerDic["type"] = self.type
-        answerDic["viewedTime"] = NSNumber(short: self.viewedTime)
+        answerDic["viewedTime"] = self.viewedTime
         return answerDic
     }
 
